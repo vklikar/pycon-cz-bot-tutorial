00235a1d139292face820ed5f98029c6c9966d0d
Václav Klikar <30387059+vklikar@users.noreply.github.com>
2019-06-16 12:14:10 +0200

    Like all the things

--- github_bot/__main__.py
+++ github_bot/__main__.py
@@ -20,6 +20,26 @@ async def on_issue_opened(
     await github_api.post(comments_api_url, data={"body": message})
 
 
+@process_event_actions('issue_comment', {'created'})
+@process_webhook_payload
+async def on_comment_posted(
+        *,
+        action, issue, comment,
+        repository=None, sender=None,
+        installation=None,
+        assignee=None, changes=None,
+):
+    """Whenever an issue is opened, greet the author and say thanks."""
+    github_api = RUNTIME_CONTEXT.app_installation_client
+    reactions_api_url = f'comment["url"]/reactions'
+   
+    await github_api.post(
+        reactions_api_url,
+        preview_api_version='squirrel-girl',
+        data={"content": "+1"},
+    )
+
+
 if __name__ == "__main__":
     run_app(
         name='PyCon-Bot-by-vklikar',
