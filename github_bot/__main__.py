a68ab01208cebf003399691b2bbf24053957d6b4
Václav Klikar <30387059+vklikar@users.noreply.github.com>
2019-06-16 11:19:54 +0200

    Add an initial bot skeleton

---
+++ github_bot/__main__.py
@@ -0,0 +1,9 @@
+from octomachinery.app.server.runner import run as run_app
+
+
+if __name__ == "__main__":
+    run_app(
+        name='PyCon-Bot-by-vklikar',
+        version='1.0.0',
+        url='https://github.com/apps/pyyyyyycoooon-booooot111',
+    )
